0f02492b856b41d82fd4dbad5fd3407aa1bd147a
Doug Perez <perezcpt@gmail.com>
2021-02-06 13:22:19 -0800

    finish owner generator

--- database/ownersGenerator.js
+++ database/ownersGenerator.js
@@ -3,12 +3,12 @@ const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const owner = (start, end) => {
   let owners = [];
-  for (let i = start; i <= end; ++i) {
+  for (let i = start; i <= end; i++) {
     let item = {
       id: i,
-      owner_name = faker.internet.userName(),
-      email = faker.internet.email(),
-      phone = faker.phone.phoneNumber()
+      owner_name: faker.internet.userName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber()
     };
     owners.push(item);
   }
@@ -16,7 +16,7 @@ const owner = (start, end) => {
 };
 
 const csvWriter = createCsvWriter({
-  path: '/Users/douglasperez/Hack-reactor/SDC/Postgress-Refactor/PhotoGallery-rev1-copy/database/owners.csv',
+  path: '/Users/douglasperez/Hack-reactor/SDC/Postgress-Refactor/PhotoGallery-rev1-copy/database/CSV/owners.csv',
   header: [
     { id: 'id', title: 'id' },
     { id: 'owner_name', title: 'owner_name' },
@@ -29,9 +29,9 @@ async function writeOwners(number) {
   const currChunk = Math.floor(number / 10);
   console.log("Number of Chunks", currChunk);
 
-  for (let i = 0; i < 10; ++i) {
+  for (let i = 0; i < 10; i++) {
     console.log(`Processing home chunk : ${i + 1}`);
-    const ownerDump = home(currChunk * i, currChunk * (i + 1) - 1);
+    const ownerDump = owner(currChunk * i, currChunk * (i + 1) - 1);
     await csvWriter.writeRecords(ownerDump);
   }
 }
